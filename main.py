3f5077da0baca5e8e0e8c1e9ce04c5292934cec7
Jose matheus <j.matheus.v.souza@gmail.com>
2025-03-31 08:36:07 -0300

    commit da terceira aula

---
+++ main.py
@@ -0,0 +1,7 @@
+nome = input ("Insira o nome do empregado: ")
+idade = input ("Insira a idade do empregado: ")
+salario = input ("Insira o salário do empregado: ")
+
+print(nome)
+print(idade)
+print(salario)
