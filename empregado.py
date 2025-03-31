c09afd28557ee9213aabe1e2593be1742bd56e0a
Jose matheus <j.matheus.v.souza@gmail.com>
2025-03-31 09:45:05 -0300

    commit de criação de operadores matematicos

--- empregado.py
+++ empregado.py
@@ -3,6 +3,6 @@ idade = int (input (f"Insira a idade do empregado: "))
 salario = float (input(f"Insira o salário do empregado: "))
 aumento = float (input (f"Insira valor de aumento do empregado: "))
 valoraumento = salario*aumento/100
-novosalario = salario+aumento
+novosalario = salario+valoraumento
 print(f"O empregado {nome} tem {idade} e recebe {salario}"
-      f"Você recebeu um aumento de {aumento} seu novo salario é de {novosalario: .2f}")
+      f" Você recebeu um aumento de {aumento} seu novo salario é de {novosalario: .2f}")
