57dc88eff652f862b381207d850d43458aa888cc
Jose matheus <j.matheus.v.souza@gmail.com>
2025-03-31 09:19:39 -0300

    exercicio de percentual

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 nome = input ("Insira o nome do empregado: ")
-idade = input ("Insira a idade do empregado: ")
-salario = input ("Insira o salário do empregado: ")
-
-print(nome)
-print(idade)
-print(salario)
+idade = int (input (f"Insira a idade do empregado: "))
+salario = float (input(f"Insira o salário do empregado: "))
+aumento = float (input (f"Insira valor de aumento do empregado: "))
+valoraumento = salario*aumento/100
+novosalario = salario+aumento
+print(f"O empregado {nome} tem {idade} e recebe {salario}"
+      f"Você recebeu um aumento de {aumento} seu novo salario é de {novosalario: .2f}")
