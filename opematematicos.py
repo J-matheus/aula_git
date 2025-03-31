f7f9c530fd207bdcf25c8c296e3f8ab6975b1ec4
Jose matheus <j.matheus.v.souza@gmail.com>
2025-03-31 09:47:02 -0300

    criação de operadores matematicos

---
+++ opematematicos.py
@@ -0,0 +1,12 @@
+n1 = int (input(f"Insira o primeiro numero: "))
+n2 = int (input(f"Insira o primeiro numero: "))
+
+soma = n1 + n2
+sub = n1 - n2
+multip = n1 * n2
+div = n1 / n2
+
+print (f"Do calculo {n1}+{n2} o valor da soma é: {soma} ")
+print (f"Do calculo {n1}-{n2} o valor da subtração é: {sub} ")
+print (f"Do calculo {n1}*{n2} o valor da multiplicação é: {multip} ")
+print (f"Do calculo {n1}/{n2} o valor da divisão é: {div} ")
