1cc01a061b95c5ec5cf4694732f072dafe062c4b
jose matheus <j.matheus.v.souza@gmail.com>
2025-04-06 13:19:54 -0300

    desafio concluido

--- entradadados.py
+++ entradadados.py
@@ -4,11 +4,9 @@ minuto1 = int(input("Digite os minutos de entrada 1:(exe: 45) "))
 hora2 = int(input("Digite a hora de entrada 2: (exe: 14) "))
 minuto2 = int(input("Digite os minutos de entrada 2: (exe: 20) "))
 
-#tudo se torna minutos
 total_minuto1 = hora1 * 60 + minuto1
 total_minuto2 = hora2 * 60 + minuto2
 
-#minutos convertidos acima
 resultado = total_minuto2 - total_minuto1 - 270
 
 if resultado <0:
@@ -21,3 +19,4 @@ if minuto_resultado <10:
     print(f"Saída: {hora_resultado}:0{minuto_resultado}")
 else:
     print(f"Saída: {hora_resultado}:{minuto_resultado}")
+    
